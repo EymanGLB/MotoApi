c8ceda1c646183656263c300d8b7389cda003257
eyman.pashaliev <eyman.pashaliev@globant.com>
2024-02-13 12:20:55 +0100

    fix: small fix card colors

--- components/atoms/Card.vue
+++ components/atoms/Card.vue
@@ -13,11 +13,11 @@ const props = defineProps({
 })
 
 const ManufacturerMap = {
-  'yamaha': 'bg-blue-100 border-blue-950 text-blue-950 dark:bg-slate-900 dark:border-blue-100 dark:text-blue-100',
-  'suzuki': 'bg-yellow-100 border-yellow-950 text-yellow-950 dark:bg-slate-900 dark:border-yellow-100 dark:text-yellow-100',
-  'kawasaki': 'bg-emerald-100 border-emerald-950 text-emerald-950 dark:bg-slate-900 dark:border-emerald-100 dark:text-emerald-100',
-  'honda': 'bg-red-100 border-red-950 text-red-950 dark:bg-slate-900 dark:border-red-100 dark:text-red-100',
-  'ktm': 'bg-orange-100 border-orange-950 text-orange-950 dark:bg-slate-900 dark:border-orange-100 dark:text-orange-100'
+  'yamaha': 'bg-blue-100 border-blue-800 text-blue-900 dark:bg-slate-900 dark:border-blue-300 dark:text-blue-200',
+  'suzuki': 'bg-yellow-100 border-yellow-800 text-yellow-900 dark:bg-slate-900 dark:border-yellow-300 dark:text-yellow-200',
+  'kawasaki': 'bg-emerald-100 border-emerald-800 text-emerald-900 dark:bg-slate-900 dark:border-emerald-300 dark:text-emerald-200',
+  'honda': 'bg-red-100 border-red-950 text-red-800 dark:bg-slate-900 dark:border-red-300 dark:text-red-200',
+  'ktm': 'bg-orange-100 border-orange-800 text-orange-900 dark:bg-slate-900 dark:border-orange-300 dark:text-orange-200'
 }
 
 </script>
@@ -25,8 +25,8 @@ const ManufacturerMap = {
 <template>
   <div
       :class="ManufacturerMap[make.toLowerCase()]"
-      class="flex flex-col justify-between px-24 py-8 shadow-lg rounded-lg items-center self-center
-      align-middle border-2 bg-slate-100 border-emerald-900 dark:bg-slate-800 dark:text-slate-200"
+      class="flex flex-col justify-between px-24 py-8 shadow-lg shadow-slate-300 rounded-lg items-center self-center
+      align-middle border-2 bg-slate-100 border-emerald-900 dark:bg-slate-800 dark:text-slate-200 dark:shadow-slate-700"
   >
     <slot />
   </div>
